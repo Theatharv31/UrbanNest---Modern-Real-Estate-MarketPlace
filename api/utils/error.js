b748d5ab9da8128b39c4c3af85dd90955461bff1
Theatharv31 <isapureatharv2003@gmail.com>
2024-06-30 15:16:47 +0530

    Create SignIn UI

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,6 @@
 export const errorHandler = (statusCode, message) => {
-    const error = new Error();
-    statusCode=statusCode;
-    message = message;
+    const error = new Error(message);
+    error.statusCode=statusCode;
+    
     return error;
 };
